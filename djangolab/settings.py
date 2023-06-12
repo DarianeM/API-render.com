63add702789bc51e8c9738ee7e53351d96aba64d
Dariane <dmedrand@espol.edu.ec>
2023-06-12 01:24:52 -0500

    Modificaciones en las librerias

--- djangolab/settings.py
+++ djangolab/settings.py
@@ -52,13 +52,13 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'djangolab.urls'
@@ -87,14 +87,6 @@ WSGI_APPLICATION = 'djangolab.wsgi.application'
 #django.db.backends.sqlite3
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': 'amst-lab',
-        'USER': 'admin',
-        'PASWORD': 'admin',
-        'HOST': 'localhost',
-        'PORT': '5432',
-    },
     'default': dj_database_url.config(
         default='sqlite:///db.sqlite3', 
         conn_max_age=600
@@ -139,6 +131,14 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+# Following settings only make sense on production and may break development environments.
+if not DEBUG:    # Tell Django to copy statics to the `staticfiles` directory
+    # in your application directory on Render.
+    STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+    # Turn on WhiteNoise storage backend that takes care of compressing static files
+    # and creating unique names for each version so they can safely be cached forever.
+    STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
