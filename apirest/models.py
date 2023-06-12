4feece007d9a2a21d766765a8da6fa3fa38cb0e7
Dariane <dmedrand@espol.edu.ec>
2023-06-12 00:21:27 -0500

    Primer commit

---
+++ apirest/models.py
@@ -0,0 +1,13 @@
+from django.db import models
+
+# Create your models here.
+class Sensores(models.Model):
+    temperatura = models.FloatField()
+    peso = models.FloatField()
+    humedad = models.IntegerField()
+
+class Lecturas(models.Model):
+    key = models.TextField()
+    value = models.FloatField()
+    data_created = models.DateTimeField(auto_now_add=True)
+    #data_updated = models.DateTimeField(auto_now=True)
