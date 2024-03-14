fc22fa30f50d6ed70255d3628f17400c7ab5235d
DarianeM <84648325+DarianeM@users.noreply.github.com>
2024-03-14 13:31:36 -0500

    Update models.py

--- apirest/models.py
+++ apirest/models.py
@@ -11,9 +11,3 @@ class Lecturas(models.Model):
     value = models.FloatField()
     data_created = models.DateTimeField(auto_now_add=True)
     #data_updated = models.DateTimeField(auto_now=True)
-
-class Pruebas(models.Model):
-    nombre = models.TextField()
-    edad = models.TextField()
-    direccion = models.TextField()
-    #data_updated = models.DateTimeField(auto_now=True)
